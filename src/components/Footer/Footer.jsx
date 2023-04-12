e7c016691cad869ed4a4d6180c640cd633b2ffc7
Tom <tom@Toms-MacBook-2.local>
2023-04-12 17:21:14 +0400

    Updated some components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import './Footer.css'
 import TasksFilter from '../TasksFilter/TasksFilter'
 
-export default class Footer extends Component {
-  static defaultProps = {
-    tasksLeft: 0,
-    filter: 'all',
-    onFilterChange: () => {},
-    onClearCompleted: () => {},
-  }
+export default function Footer(props) {
+  const { tasksLeft, filter, onFilterChange, onClearCompleted } = props
 
-  static propTypes = {
-    tasksLeft: PropTypes.number,
-    filter: PropTypes.string,
-    onFilterChange: PropTypes.func,
-    onClearCompleted: PropTypes.func,
-  }
+  return (
+    <footer className="footer">
+      <span className="task-count">{tasksLeft} items left</span>
+      <TasksFilter filter={filter} onFilterChange={onFilterChange} />
+      <button className="clear-completed" onClick={onClearCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  )
+}
 
-  render() {
-    const { tasksLeft, filter, onFilterChange, onClearCompleted } = this.props
+Footer.defaultProps = {
+  tasksLeft: 0,
+  onClearCompleted: () => {},
+}
 
-    return (
-      <footer className="footer">
-        <span className="task-count">{tasksLeft} items left</span>
-        <TasksFilter filter={filter} onFilterChange={onFilterChange} />
-        <button className="clear-completed" onClick={onClearCompleted}>
-          Clear completed
-        </button>
-      </footer>
-    )
-  }
+Footer.propTypes = {
+  tasksLeft: PropTypes.number,
+  onClearCompleted: PropTypes.func,
 }
--- src/components/TasksFilter/TasksFilter.jsx
+++ src/components/TasksFilter/TasksFilter.jsx
@@ -1,47 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import './TasksFilter.css'
 
-export default class TasksFilter extends Component {
-  static defaultProps = {
-    filter: 'all',
-    onFilterChange: () => {},
-  }
-
-  static propTypes = {
-    filter: PropTypes.string,
-    onFilterChange: PropTypes.func,
-  }
-
-  buttons = [
+export default function TasksFilter(props) {
+  const { filter, onFilterChange } = props
+  const buttons = [
     { name: 'all', label: 'All' },
     { name: 'active', label: 'Active' },
     { name: 'completed', label: 'Completed' },
   ]
 
-  render() {
-    const { filter, onFilterChange } = this.props
+  const btn = buttons.map(({ name, label }) => {
+    const isActive = filter === name
+    const clazz = isActive ? 'taskfilter_btn--select' : 'taskfilter_btn--noselect'
+    return (
+      <li key={name}>
+        <label className={`taskfilter_btn ${clazz}`}>
+          {label}
+          <input
+            type="radio"
+            className="taskfilter_input"
+            checked={isActive}
+            readOnly
+            onClick={() => onFilterChange(name)}
+          ></input>
+        </label>
+      </li>
+    )
+  })
 
-    const buttons = this.buttons.map(({ name, label }) => {
-      const isActive = filter === name
-      const clazz = isActive ? 'taskfilter_btn--select' : 'taskfilter_btn--noselect'
-      return (
-        <li key={name}>
-          <label className={`taskfilter_btn ${clazz}`}>
-            {label}
-            <input
-              type="radio"
-              className="taskfilter_input"
-              checked={isActive}
-              readOnly
-              onClick={() => onFilterChange(name)}
-            ></input>
-          </label>
-        </li>
-      )
-    })
+  return <ul className="taskfilter">{btn}</ul>
+}
 
-    return <ul className="taskfilter">{buttons}</ul>
-  }
+TasksFilter.defaultProps = {
+  filter: 'all',
+  onFilterChange: () => {},
+}
+
+TasksFilter.propTypes = {
+  filter: PropTypes.string,
+  onFilterChange: PropTypes.func,
 }
